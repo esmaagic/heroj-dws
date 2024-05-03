d3f56f2ce428d46822995ff75bc806793380349a
Fatma Hana <fatmamemisevic2001@gmail.com>
2024-05-03 16:05:01 +0200

    Replacing hardcoded response

--- backend/ai.py
+++ backend/ai.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+# import json
 
 # Loading environment variables from .env file
 load_dotenv()
@@ -11,10 +12,31 @@ api_key = os.getenv("API_KEY")
 # Initializing the OpenAI client with our API key
 client = OpenAI(api_key=api_key)
 
+"""
 response = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-         {"role": "user", "content": "List top 5 anime of all time"}
+         {"role": "user", "content": "List top 1 anime of all time"}
     ]
 )
-print(response)
+
+response_dict = response.to_dict()
+response_json = json.dumps(response_dict, indent=4)
+"""
+
+chat_log = []
+
+while True:
+    user_message = input()
+    if user_message.lower() == "quit":
+        break
+    else:
+        chat_log.append({"role": "user", "content": user_message})
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=chat_log
+        )
+
+    assistant_response = response.choices[0].message.content
+    print("AI: ", assistant_response)
+    chat_log.append({"role": "assistant", "content": assistant_response})
