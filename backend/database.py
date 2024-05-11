2f5bdcc8fb109ee6b2e86360452fff99904ca7fd
Esma <108765837+esmaagic@users.noreply.github.com>
2024-05-11 13:36:09 +0200

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/db"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
